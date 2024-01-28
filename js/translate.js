7284fbad17fd3dd5c8520b3cc23d72f6f449ce64
Yash Bhardwaj <97777995+yashbhardwaj11@users.noreply.github.com>
2024-01-28 17:54:46 +0530

    translate

---
+++ js/translate.js
@@ -0,0 +1,77 @@
+function translate(language) {
+  console.log(language);
+  fetch(`languages/${language}.json`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(
+          `Failed to fetch translations (${response.status}): ${response.statusText}`
+        );
+      }
+      return response.json();
+    })
+    .then((translations) => {
+      document.querySelectorAll("[data-translate]").forEach((element) => {
+        const key = element.dataset.translate;
+        element.textContent = translations[key] || key;
+      });
+
+      // Save selected language as a cookie
+      document.cookie = `selectedLanguage=${language}; path=/`;
+    })
+    .catch((error) => console.error(error));
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("#form");
+  const submitButton = form.querySelector("button[type='submit']");
+  const scriptURL =
+    "https://script.google.com/macros/s/AKfycbz4bRvifpz1OYxBv4fCZPSV3i9fsm-yXtSYpVjH9Eod4kLtpseEYTfpccmkPG00gE5f/exec";
+
+  const selectedLanguage = getCookie("selectedLanguage");
+  if (selectedLanguage) {
+    translate(selectedLanguage);
+  }
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    submitButton.disabled = true;
+
+    fetch(scriptURL, {
+      method: "POST",
+      body: new FormData(form),
+    })
+      .then((response) => {
+        if (response.ok) {
+          alert("Success!");
+          form.reset();
+        } else {
+          alert("Error! Please try again.");
+        }
+        submitButton.disabled = false;
+      })
+      .catch((error) => {
+        alert("Error! Please try again.");
+        submitButton.disabled = false;
+      });
+  });
+
+  const languageSelector = document.getElementById("languageSelector");
+
+  languageSelector.addEventListener("change", function () {
+    translate(this.value);
+  });
+
+  window.translate = translate; 
+
+  function getCookie(name) {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      if (cookie.startsWith(`${name}=`)) {
+        return cookie.substring(name.length + 1);
+      }
+    }
+    return null;
+  }
+});
